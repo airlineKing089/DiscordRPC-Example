7a5aae7e25934322ab03430b9604e6d7cb3f90fd
airlineKing089 <74291523+airlineKing089@users.noreply.github.com>
2022-06-23 18:14:22 +0100

    Reworked the example

--- presence template.py
+++ presence template.py
@@ -1,11 +1,20 @@
-from pypresence import Presence
+# import modules
 import time
+from pypresence import Presence
 
-client_id = 'YOU CLIENT ID HERE' #Put your client ID here
-RPC = Presence(client_id) 
-RPC.connect() 
-
-print(RPC.update(state="STATE HERE", details="DETAILS HERE", large_image="NAME OF LARGE IMAGE HERE", small_image="NAME OF SMALL IMAGE HERE", large_text="LARGE IMAGE TEXT HERE", start=time.time()))  # Set the presence
-
-while True: 
-    time.sleep(15) 
+class DiscordRPC():
+    def RPC_Start(client_id):
+        if isinstance(client_id) is True:
+            # connect to the RPC
+            RPC = Presence(client_id)
+            RPC.connect()
+            
+            # updates the rpc
+            print(RPC.update(state="STATE HERE", details="DETAILS HERE", large_image="NAME OF LARGE IMAGE HERE", small_image="NAME OF SMALL IMAGE HERE", large_text="LARGE IMAGE TEXT HERE", start=time.time()))
+            
+            # while loop to keep it running
+            while True:
+                time.sleep(15)
+               
+    def RPC_Exit():
+        Presence.close()
