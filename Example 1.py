a4cf48bee4b0bf233426a2d8763b50187371455b
airlineKing089 <74291523+airlineKing089@users.noreply.github.com>
2022-06-23 18:46:19 +0100

    Updated the example
    Added user input for the settings

--- Example 1.py
+++ Example 1.py
@@ -2,15 +2,22 @@
 import time
 from pypresence import Presence
 
+client_id = input("Client ID: ")
+state = input("State: ")
+details = input("Details: ")
+large_image = input("Large Image Name: ")
+small_image = input("Small Image Name: ")
+large_image_text = input("Large Image Details: ")
+
 class DiscordRPC():
-    def RPC_Start(client_id):
+    def RPC_Start(client_id, state, details, large_image, small_image, large_image_text):
         if isinstance(client_id) is True:
             # connect to the RPC
             RPC = Presence(client_id)
             RPC.connect()
             
             # updates the rpc
-            print(RPC.update(state="STATE HERE", details="DETAILS HERE", large_image="NAME OF LARGE IMAGE HERE", small_image="NAME OF SMALL IMAGE HERE", large_text="LARGE IMAGE TEXT HERE", start=time.time()))
+            print(RPC.update(state=state, details=details, large_image=large_image, small_image=small_image, large_text=large_image_text, start=time.time()))
             
             # while loop to keep it running
             while True:
@@ -18,3 +25,7 @@ class DiscordRPC():
                
     def RPC_Exit():
         Presence.close()
+
+drpc = DiscordRPC()
+
+drpc.RPC_Start(client_id, state, details, large_image, small_image, large_image_text)
